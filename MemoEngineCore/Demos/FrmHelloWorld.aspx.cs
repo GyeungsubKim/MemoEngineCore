c8b74512f927c175bef718aeabdaf8459dacbdd8
thegreatman <thegreatman@naver.com>
2020-08-27 10:30:21 +0900

    초기 커밋.

---
+++ MemoEngineCore/Demos/FrmHelloWorld.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MemoEngineCore
+{
+    public partial class FrmHelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BnClick_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = txtInput.Text;
+            txtInput.Text = string.Empty;
+        }
+    }
+}
